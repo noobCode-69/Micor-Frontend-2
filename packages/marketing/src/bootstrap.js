f8bf29b4accd7dc7f6d492e226e1c8916916a067
so_hell <armanyppph@gmail.com>
2023-05-04 13:43:29 +0530

    cleanup

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -9,6 +9,7 @@ const mount = (el) => {
 
 // If we are in development and in isolation
 // call mount immdiately
+
 if (process.env.NODE_ENV === "development") {
   const root = document.querySelector("#_marketing-dev-root");
   if (root) {
